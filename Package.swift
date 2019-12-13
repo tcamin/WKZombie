b88d885464e349092f96745acdb5b445a91d669f
Tomas Camin <tomas.camin@adevinta.com>
2019-12-13 12:10:21 +0100

    Update package file

--- Package.swift
+++ Package.swift
@@ -1,12 +1,16 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "WKZombie",
-  targets: [
-      Target(name: "WKZombie"),
-      Target(name: "Example", dependencies:["WKZombie"])
-  ],
-  dependencies: [
-	   .Package(url: "https://github.com/mkoehnke/hpple.git", Version(0,2,2))
-  ]
+    name: "WKZombie",
+    dependencies: [
+      .package(url: "https://github.com/mkoehnke/hpple.git", .branch("master"))
+    ],
+    targets: [
+      .target(
+          name: "WKZombie",
+          dependencies: [])
+    ]
 )
